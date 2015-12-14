4ab7dbd44f50afb34af110164f3bdd40b8e5528a
Agil Mahendra <agil.mahendra@gmail.com>
2015-12-14 09:06:56 +0700

    styling & js dashboard & balin.id #3

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,21 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.less('app.less');
+	mix.sass('admin/dashboard.scss', 'public/css/dashboard.css')
+		.sass('web/balin.scss', 'public/css/balin.css')
+		.scripts(['global/jquery.js', 
+				'global/bootstrap.min.js', 
+				'admin/dynamicForm.js', 
+				'admin/metisMenu.min.js'
+				], 'public/js/dashboard.js')
+		.scripts(['global/jquery.js', 
+				'global/bootstrap.min.js'
+				], 'public/js/balin.js')
+		.version(['public/css/dashboard.css', 
+				'public/css/balin.css',
+				'public/js/dashboard.js',
+				'public/js/balin.js'])
+		.copy('resources/assets/fonts', 'public/build/fonts/')
+		.copy('resources/assets/plugins/', 'public/plugins/')
+		.copy('resources/assets/images/', 'public/images/');
 });
