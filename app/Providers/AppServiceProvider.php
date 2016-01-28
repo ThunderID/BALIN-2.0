bcdb9fbc7796ed3643850ce0243580143670adbb
Agil Mahendra <agil.mahendra@gmail.com>
2016-01-28 20:18:57 +0700

    add blade extends & disabled verify token missmatch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Blade;
+use Blade, Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,11 +14,29 @@ class AppServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
-		//
+		// Blade extens date indonesia
+        Blade::directive('date_indo', function($expression) 
+        {
+            return "<?php echo Carbon::parse($expression)->format('d-m-Y'); ?>";
+        });
+
+        // blade extends date time indonesia with name month 
+        Blade::directive('datetime_indo_with_name_month', function($expression)
+        {
+        	return "<?php echo Carbon::parse($expression)->format('d F Y  |  H:i'); ?>";
+        });
+        
+		// blade extens money indonesia
 		Blade::directive('money_indo', function($expression)
 		{
 			return "<?php echo 'IDR '.number_format($expression, 0, ',', '.'); ?>";
 		});
+
+		// blade extens money indonesia for pengenal pembayaran
+		Blade::directive('money_indo_negative', function($expression)
+        {
+            return "<?php echo 'IDR -'.number_format($expression, 0, ',', '.'); ?>";
+        });
 	}
 
 	/**
