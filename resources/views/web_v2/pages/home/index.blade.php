64a9739cf5254cae58c1961bd61e491d446be81b
Agil Mahendra <agil.mahendra@gmail.com>
2016-02-04 19:33:03 +0700

    update home with button_link_url

--- resources/views/web_v2/pages/home/index.blade.php
+++ resources/views/web_v2/pages/home/index.blade.php
@@ -7,7 +7,10 @@
 			<ul>
 				<!-- SLIDE 1-->
 				@forelse($balin['sliders'] as $key => $value)
-					<li data-transition="fade" data-slotamount="5" data-masterspeed="700" data-link="#"
+					<?php 
+						$slider = json_decode($value['value'], 200);
+					?>
+					<li data-transition="fade" data-slotamount="5" data-masterspeed="700" data-link="{{ isset($slider['button']['slider_button_url']) ? $slider['button']['slider_button_url'] : '#' }}"
 						<!-- MAIN IMAGE -->
 						<img src="{!! $value['image']['image_lg'] !!}"   alt="slidebg1"  data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
 					</li>
@@ -19,11 +22,13 @@
 	<section class="container-fluid hidden-md hidden-lg" style="margin-top:51px">
 		<div class="row">
 			@forelse($balin['sliders'] as $key => $value)
-				<?php $i = 0; ?>
+				<?php 
+					$slider = json_decode($value['value'], 200);
+				?>
 				<div class="col-xs-12 pl-0 pr-0 border-bottom" style="position:relative;">
 					<div class="caption-mobile">
 					</div>
-					<a href="#">
+					<a href="{{ isset($slider['button']['slider_button_url']) ? $slider['button']['slider_button_url'] : '#' }}">
 						<img src="{!! $value['image']['image_lg'] !!}" class="img-responsive" style="width:100%;">
 					</a>
 				</div>
