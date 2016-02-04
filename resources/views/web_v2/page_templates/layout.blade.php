682dc44fe0622aa4965e13ee9773d1bbb216fe49
Agil Mahendra <agil.mahendra@gmail.com>
2016-02-04 10:54:47 +0700

    modify route error

--- resources/views/web_v2/page_templates/layout.blade.php
+++ resources/views/web_v2/page_templates/layout.blade.php
@@ -38,7 +38,7 @@
 		<!-- END SECTION NAVBAR -->
 
 		<!--SECTION WRAPPER -->
-		<div class="wrapper @yield('wrapper_class')" style=" margin-bottom: 0px;
+		<div class="wrapper wrapper_content @yield('wrapper_class')" style=" margin-bottom: 0px;
 			{{ (Route::currentRouteName()!='balin.home.index') ? 'margin-top:51px;' : 'margin-top:0px;' }}  ">
 			<section class="{{ (Route::currentRouteName()!='balin.home.index') ? 'container' : '' }}">
 				@if (Route::currentRouteName()!='balin.home.index')
@@ -57,35 +57,35 @@
 		<!-- END SECTION WRAPPER -->
 
 		<!-- SECTION BOTTOM BAR FOR MOBILE HOME, PRODUCT & PROFILE -->
-		<div class="navbar navbar-default navbar-fixed-bottom navbar hidden-lg hidden-md hidden-sm col-xs-12 border-top-1 border-grey" role="navigation">
+		<div class="navbar navbar-default navbar-fixed-bottom navbar_shortcut hidden-lg hidden-md hidden-sm col-xs-12 border-top-1 border-grey" role="navigation">
 			<div class="nav navbar-nav text-center mt-0 mb-0">
 				<div onclick="location.href='{{ URL::route('balin.home.index') }}';" class="col-xs-{{ (Session::has('whoami')) ? "3" : "4" }} text-center border-right-1 border-grey pt-xs pb-xs">
-					{!! HTML::image('images/home.png', 'image' ,['style' => 'height:37px; width:25px; margin: 0 auto;', 'class' => 'pt-5 pb-5']) !!}
+					{!! HTML::image('images/home.png', 'image' ,['style' => 'height:35px; width:25px; margin: 0 auto;', 'class' => 'pt-5 pb-5']) !!}
 					<p class="text-sm">Home</p>
 				</div>
-				<div onclick="location.href='{{ URL::route('balin.product.index') }}';" class="col-xs-{{ (Session::has('whoami')) ? "3" : "4" }} text-center border-right-1 border-grey pt-xs pb-xs" style="height:75px;">
-					{!! HTML::image('images/product.png', 'image' ,['style' => 'height:37px; width:25px; margin: 0 auto;', 'class' => 'pt-5 pb-5']) !!}
+				<div onclick="location.href='{{ URL::route('balin.product.index') }}';" class="col-xs-{{ (Session::has('whoami')) ? "3" : "4" }} text-center border-right-1 border-grey pt-xs pb-xs">
+					{!! HTML::image('images/product.png', 'image' ,['style' => 'height:35px; width:25px; margin: 0 auto;', 'class' => 'pt-5 pb-5']) !!}
 					<p class="text-sm">Produk</p>
 				</div>
 				@if (Session::has('whoami'))
-					<div onclick="location.href='{{ URL::route('balin.profile') }}';" class="col-xs-3 text-center border-right-1 border-grey pt-xs pb-xs" style="height:75px;">
-						{!! HTML::image('images/profile.png', 'image', ['style' => 'height:37px; width:25px; margin: 0 auto;', 'class' => 'pt-5 pb-5']) !!}
+					<div onclick="location.href='{{ URL::route('balin.profile') }}';" class="col-xs-3 text-center border-right-1 border-grey pt-xs pb-xs">
+						{!! HTML::image('images/profile.png', 'image', ['style' => 'height:35px; width:25px; margin: 0 auto;', 'class' => 'pt-5 pb-5']) !!}
 						<p class="text-sm">Profile</p>
 					</div>
 				@endif
 				@if (Session::has('whoami'))
-					<div onclick="location.href='{{ URL::route('balin.get.logout') }}';" class="col-xs-3 text-center pt-xs pb-xs" style="height:75px;">
-						{!! HTML::image('images/sign-out.png', 'image' ,['style' => 'height:37px; width:25px; margin: 0 auto;', 'class' => 'pt-5 pb-5']) !!}
+					<div onclick="location.href='{{ URL::route('balin.get.logout') }}';" class="col-xs-3 text-center pt-xs pb-xs">
+						{!! HTML::image('images/sign-out.png', 'image' ,['style' => 'height:35px; width:25px; margin: 0 auto;', 'class' => 'pt-5 pb-5']) !!}
 				@else
-					<div onclick="location.href='{{ URL::route('balin.get.login') }}';" class="col-xs-4 text-center pt-xs pb-xs" style="height:75px;">
-						{!! HTML::image('images/sign-in.png', 'image' ,['style' => 'height:37px; width:25px; margin: 0 auto;', 'class' => 'pt-5 pb-5']) !!}
+					<div onclick="location.href='{{ URL::route('balin.get.login') }}';" class="col-xs-4 text-center pt-xs pb-xs">
+						{!! HTML::image('images/sign-in.png', 'image' ,['style' => 'height:35px; width:25px; margin: 0 auto;', 'class' => 'pt-5 pb-5']) !!}
 				@endif
 					<p class="text-sm">{{ (Session::has('whoami')) ? "Log Out":"Sign In" }}</p>
 				</div>		
 			</div>
 		</div>
 		<!-- END SECTION BOTTOM BAR FOR MOBILE HOME, PRODUCT & PROFILE -->
-
+		<div class="divider_footer"></div>
 		@if (Route::currentRouteName()!='balin.home.index')
 			<!-- SECTION FOOTER  -->
 			@include('web_v2.components.footer')
@@ -100,6 +100,23 @@
 		@yield('js_plugin')
 		<script type="text/javascript">
 			@yield('js')
+			$(document).ready(function(){
+				console.log($('.wrapper_content').height());
+				console.log($('.divider_footer').position().top);
+				wrapper_height = $('.wrapper_content').height();
+				$(function () {
+
+					$(window).scroll( function() {
+						if ($(this).scrollTop() >= wrapper_height) {
+							$('.navbar_shortcut').fadeOut();
+						}
+						else {
+							$('.navbar_shortcut').fadeIn();
+						}
+					});
+				});
+			})
+			
 		</script>
 	</body>
 </html>
