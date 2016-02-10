a0cb7ed819e934f04d9dfd2cd0dcc4dbceda1f95
Agil Mahendra <agil.mahendra@gmail.com>
2016-02-10 10:10:59 +0700

    revisi product carousel

--- public/build/js/balin-be4e29d996.js
+++ public/build/js/balin-be4e29d996.js
@@ -4341,12 +4341,12 @@ $('.owl-carousel').owlCarousel({
     navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>' ],
     responsive:{
         0:{
-            items:2,
+            items:1,
             nav:true
         },
-        600:{
-            items:3,
-            nav:false
+        768:{
+            items:1,
+            nav:true
         },
         1000:{
             items:4,
