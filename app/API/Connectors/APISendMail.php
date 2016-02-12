71a50633415ec33316a2321462610723663709c8
chelsymooy <chelsymooy1108@gmail.com>
2016-02-12 13:15:09 +0700

    change mail scheme

--- app/API/Connectors/APISendMail.php
+++ app/API/Connectors/APISendMail.php
@@ -8,12 +8,11 @@ class APISendMail extends APIData
 		parent::__construct();
 
 		$this->api->timeout 				= 10;
-		$this->api->basic_url				= 'localhost:9000';
 	}
 
 	public function welcomemail($user, $store)
 	{
-		$this->api_url 						= '/shop/welcome';
+		$this->api_url 						= '/mail/welcome';
 
 		$this->api_data 					= array_merge($this->api_data, ["user" => $user, "store" => $store]);
 
@@ -22,7 +21,7 @@ class APISendMail extends APIData
 
 	public function resetpassword($user, $store)
 	{
-		$this->api_url 						= '/shop/password/reset';
+		$this->api_url 						= '/mail/password/reset';
 
 		$this->api_data 					= array_merge($this->api_data, ["user" => $user, "store" => $store]);
 
@@ -31,7 +30,7 @@ class APISendMail extends APIData
 
 	public function invoice($invoice, $store)
 	{
-		$this->api_url 						= '/shop/invoice';
+		$this->api_url 						= '/mail/invoice';
 
 		$this->api_data 					= array_merge($this->api_data, ["invoice" => $invoice, "store" => $store]);
 
@@ -40,7 +39,7 @@ class APISendMail extends APIData
 
 	public function cancelorder($order, $store)
 	{
-		$this->api_url 						= '/shop/canceled';
+		$this->api_url 						= '/mail/canceled';
 
 		$this->api_data 					= array_merge($this->api_data, ["order" => $order, "store" => $store]);
 
