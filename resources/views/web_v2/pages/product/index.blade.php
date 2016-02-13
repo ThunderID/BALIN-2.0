7b97f8f42667293bdd3f3b7321cd6d2d4a285e3f
Agil Mahendra <agil.mahendra@gmail.com>
2016-02-13 12:00:28 +0700

    - product index modify filter use icon color, border and button search - product show change choice size, full width image product

--- resources/views/web_v2/pages/product/index.blade.php
+++ resources/views/web_v2/pages/product/index.blade.php
@@ -1,10 +1,13 @@
+<?php 
+// dd($data['tag'][0]);
+?>
 @extends('web_v2.page_templates.layout')
 
 @section('content')
 	<!-- SECTION MENU CATEGORIES, FILTERS, SORT BY, & SEARCH DESKTOP -->
 	<div class="row hidden-xs hidden-sm">
 		<div class="col-md-12 col-lg-12">
-			<div class="row ml-0 mr-0 border-2 border-solid">
+			<div class="row ml-0 mr-0 border-1 border-solid">
 				<!-- SECTION MENU CATEGORIES, FILTER & SORT BY -->
 				<div class="col-md-9 col-lg-9 filter-product pl-5">
 					@include('web_v2.components.product.menu_product.filter_desktop')
@@ -12,7 +15,7 @@
 				<!-- END SECTION MENU CATEGORIES, FILTER & SORT BY -->
 
 				<!-- SECTION FORM SEARCHING -->
-				<div class="col-md-3 col-lg-3">
+				<div class="col-md-3 col-lg-3 pr-0">
 					@include('web_v2.components.product.menu_product.searching_desktop')
 				</div>																								
 				<!-- END SECTION FORM SEARCHING -->
@@ -25,9 +28,9 @@
 						<ul class="list-inline">
 							@foreach ($data['category'][0] as $k => $v)
 								<div class="col-md-3 col-sm-4 pl-0">
-									<li class="mr-lg pt-sm pb-sm ml-5 @if(Input::get('category')==$v['slug']) active @endif">
-										<a href="{{ route('balin.product.index', array_merge(Input::all(), ['category' => $v['slug']])) }}">{{ $v['name'] }}</a>
-									</li>
+									<a href="{{ route('balin.product.index', array_merge(Input::all(), ['category' => $v['slug']])) }}">
+										<li class="pt-sm pb-sm @if(Input::get('category')==$v['slug']) active @endif">{{ $v['name'] }}</li>
+									</a>
 								</div>
 							@endforeach
 						</ul>
@@ -53,9 +56,14 @@
 								@foreach ($data['tag'][0] as $k2 => $v2)
 									@if ($v['category_id'] == $v2['id'])
 										<div class="col-md-3 col-sm-4 pl-0">
-											<li class="mr-lg p-sm ml-sm @if(Input::get('tag')==$v['slug']) active @endif">
-												<a href="{{ route('balin.product.index', array_merge(Input::all(), ['tag' => $v['slug']])) }}">{{ $v['name'] }}</a>
-											</li>
+											<a href="{{ route('balin.product.index', array_merge(Input::all(), ['tag' => $v['slug']])) }}">
+												<li class="p-sm ml-sm @if(Input::get('tag')==$v['slug']) active @endif" style="position: relative">
+													<span class="pr-sm" style="height: 15px;
+													    width: 15px;
+													    background-color: {{ isset($v['code']) ? $v['code'] : '' }};"></span> &nbsp;
+													{{ $v['name'] }}
+												</li>
+											</a>
 										</div>
 									@endif
 								@endforeach
@@ -74,34 +82,34 @@
 					<div class="row sort">
 						<ul class="list-inline">
 							<div class="col-md-3 col-lg-3 pl-0">
-								<li class="mr-lg p-sm ml-5 {{ (Input::get('sort')=='name-asc') ? 'active' : '' }}">
-									<a href="{{ route('balin.product.index', array_merge(Input::all(), ['sort' => 'name-asc'])) }}" class="hover-black">Nama Produk A-Z</a>
-								</li>
+								<a href="{{ route('balin.product.index', array_merge(Input::all(), ['sort' => 'name-asc'])) }}">
+									<li class="pt-sm pb-sm {{ (Input::get('sort')=='name-asc') ? 'active' : '' }}">Nama Produk A-Z</li>
+								</a>
 							</div>
 							<div class="col-md-3 col-lg-3 pl-0">
-								<li class="mr-lg p-sm ml-5 {{ (Input::get('sort')=='name-desc') ? 'active' : '' }}">
-									<a href="{{ route('balin.product.index', array_merge(Input::all(), ['sort' => 'name-desc'])) }}" class="hover-black">Nama Produk Z-A</a>
-								</li>
+								<a href="{{ route('balin.product.index', array_merge(Input::all(), ['sort' => 'name-desc'])) }}">
+									<li class="pt-sm pb-sm {{ (Input::get('sort')=='name-desc') ? 'active' : '' }}">Nama Produk Z-A</li>
+								</a>
 							</div>
 							<div class="col-md-3 col-lg-3 pl-0">
-								<li class="mr-lg p-sm ml-5 {{ (Input::get('sort')=='price-asc') ? 'active' : '' }}">
-									<a href="{{ route('balin.product.index', array_merge(Input::all(), ['sort' => 'price-asc'])) }}" class="hover-black">Harga Produk Termurah</a>
-								</li>
+								<a href="{{ route('balin.product.index', array_merge(Input::all(), ['sort' => 'price-asc'])) }}">
+									<li class="pt-sm pb-sm {{ (Input::get('sort')=='price-asc') ? 'active' : '' }}">Harga Produk Termurah</li>
+								</a>
 							</div>
 							<div class="col-md-3 col-lg-3 pl-0">
-								<li class="mr-lg p-sm ml-5 {{ (Input::get('sort')=='price-desc') ? 'active' : '' }}">
-									<a href="{{ route('balin.product.index', array_merge(Input::all(), ['sort' => 'price-desc'])) }}" class="hover-black">Harga Produk Termahal</a>
-								</li>
+								<a href="{{ route('balin.product.index', array_merge(Input::all(), ['sort' => 'price-desc'])) }}">
+									<li class="pt-sm pb-sm {{ (Input::get('sort')=='price-desc') ? 'active' : '' }}">Harga Produk Termahal</li>
+								</a>
 							</div>
 							<div class="col-md-3 col-lg-3 pl-0">
-								<li class="mr-lg p-sm ml-5 {{ (Input::get('sort')=='newest-desc') ? 'active' : '' }}">
-									<a href="{{ route('balin.product.index', array_merge(Input::all(), ['sort' => 'newest-desc'])) }}" class="hover-black">Produk Terbaru</a>
-								</li>
+								<a href="{{ route('balin.product.index', array_merge(Input::all(), ['sort' => 'newest-desc'])) }}">
+									<li class="pt-sm pb-sm {{ (Input::get('sort')=='newest-desc') ? 'active' : '' }}">Produk Terbaru</li>
+								</a>
 							</div>
 							<div class="col-md-3 col-lg-3 pl-0">
-								<li class="mr-lg p-sm ml-5 {{ (Input::get('sort')=='newest-asc') ? 'active' : '' }}">
-									<a href="{{ route('balin.product.index', array_merge(Input::all(), ['sort' => 'newest-asc'])) }}" class="hover-black">Produk Terlama</a>
-								</li>
+								<a href="{{ route('balin.product.index', array_merge(Input::all(), ['sort' => 'newest-asc'])) }}">
+									<li class="pt-sm pb-sm {{ (Input::get('sort')=='newest-asc') ? 'active' : '' }}">Produk Terlama</li>
+								</a>
 							</div>																			
 							
 						</ul>
