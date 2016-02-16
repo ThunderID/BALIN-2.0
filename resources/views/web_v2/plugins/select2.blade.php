7a5a26913afb5c2b6c091e0055dbc8747520cffe
Agil Mahendra <agil.mahendra@gmail.com>
2016-02-16 14:41:48 +0700

    modify select2 from 3.5.4 to 4.0.1 and plugin select2

--- resources/views/web_v2/plugins/select2.blade.php
+++ resources/views/web_v2/plugins/select2.blade.php
@@ -1,12 +1,28 @@
 {!! HTML::style('plugins/select2/select2.css') !!}
-{!! HTML::style('plugins/select2/select2-bootstrap.css') !!}
 {!! HTML::script('plugins/select2/select2.min.js') !!}
 
 <script type="text/javascript">
-	$('.select_tag_email').select2({
-		placeholder: 'Tambah email',
-		tags: true,
-		tokenSeparators: [",", " "]
-	});	
-	// $('.select-tag').select2('data', preload_data_tag);
+	$(document).ready(function() {
+		$('.select_tag_email').select2({
+			placeholder: 'Tambah email teman',
+			minimumResultsForSearch: Infinity,
+			tags: true,
+			tokenSeparators: [',', ' ', '\n', '\t', ';'],
+			createTag: function(term, data) {
+			    var value = term.term;
+			    if(validateEmail(value)) {
+			        return {
+			          id: value,
+			          text: value
+			        };
+			    }
+			    return null;            
+			}
+
+		});	
+		function validateEmail(email) {
+		    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		    return re.test(email);
+		}
+	});
 </script>
