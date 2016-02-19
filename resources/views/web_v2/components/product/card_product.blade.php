c95e383a981444ac24dc13be51221e844af010aa
Agil Mahendra <agil.mahendra@gmail.com>
2016-02-19 07:57:06 +0700

    modified card product add margin bottom

--- resources/views/web_v2/components/product/card_product.blade.php
+++ resources/views/web_v2/components/product/card_product.blade.php
@@ -11,7 +11,7 @@
 					<a href="{{ route('balin.product.show', $value['slug']) }}" class="hover-grey-dark {{ isset($text) ? $text : 'text-light text-lg' }}">{{ (!empty($value['name']) ? $value['name'] : '') }}</a>
 					<p class="mb-0">@money_indo(($value['promo_price']!=0 ? $value['promo_price'] : $value['price']))</p>
 					@if ($value['promo_price'] != 0)
-						<p class="mtm-sm mb-5">
+						<p class="mtm-sm mb-0">
 							<span class="text-regular text-strikethrough mtm-md">@money_indo( $value['price'] )</span>
 						</p>
 					@endif
